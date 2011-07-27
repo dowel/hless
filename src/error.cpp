9fa06cd3e928cf6be8942c410c38ba94c95a210c
Alexander Sandler <alex@alexonlinux.com>
2011-07-27 23:09:07 +0300

    Implemented backtrace class - now printing backtrace of every exception

---
+++ src/error.cpp
@@ -0,0 +1,14 @@
+#include "error.h"
+
+std::ostream& operator<<(std::ostream& os, const Backtrace& bt)
+{
+	os << std::hex << std::showbase;
+	for (int i = 0; i < bt._n; i++) {
+		os << bt._ptrs[i] << " ";
+	}
+	
+	os << std::endl;
+	os << std::dec << std::noshowbase;
+	return os; 
+}
+
--- src/error.h
+++ src/error.h
@@ -5,21 +5,57 @@
 #include <iostream>
 #include <sstream>
 
+#include <stdlib.h>
+#include <execinfo.h>
+
+class Backtrace
+{
+public:
+	Backtrace() 
+	{
+		_n = backtrace(_ptrs, 38);
+	}
+
+	~Backtrace()
+	{
+		_n = 0;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Backtrace& bt);
+
+private:
+	void* _ptrs[38];
+	int _n;
+};
+
 class Exception : public std::exception
 {
 public:
-	Exception() : _error("General error") { }
-	Exception(std::string error) : _error(error) { }
+	Exception()
+	{ 
+		_ss << "General error" << std::endl; 
+		_ss << "At: " << Backtrace() << std::endl;
+	}
+	Exception(std::string error)
+	{
+		_ss << error.c_str() << std::endl;
+		_ss << "At: " << Backtrace() << std::endl;
+	}
+
+	Exception(const Exception& other)
+	{
+		_ss << other._ss.str().c_str();
+	}
 
     virtual ~Exception() throw() { }
 
 	virtual const char* what() const throw()
 	{
-        return _error.c_str();
+		return _ss.str().c_str();
 	}
 
 private:
-    std::string _error;
+	std::stringstream _ss;
 };
 
 #endif
